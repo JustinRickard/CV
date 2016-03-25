5d031385af92b270b8bf55a47a56319c492b4f3f
Justin Rickard <justinr86.jr@gmail.com>
2016-03-25 20:13:25 +0000

    Remove JS output files from source control

--- public/scripts/Job.js
+++
@@ -1,9 +0,0 @@
-var Job = (function () {
-    function Job(company, start, end, isCurrent) {
-        this.Company = company;
-        this.Start = start;
-        this.End = end;
-        this.IsCurrent = isCurrent;
-    }
-    return Job;
-})();
