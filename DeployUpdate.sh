6db66c61980975dd9760dee4eb7cd8048bd206f4
Justin Rickard <justinr86.jr@gmail.com>
2016-04-29 21:20:14 +0100

    Add check to deployment update script to check if any updates are due

--- DeployUpdate.sh
+++ DeployUpdate.sh
@@ -9,8 +9,15 @@ workingDirectory=~/websites
 githubUrl=https://github.com/JustinRickard/CV.git
 
 # Check if up-to-date
-#result=`git status -u no`
 
+result=$(git fetch --dry-run 2>&1)
+if [[ $result == *"master"* ]]
+then
+	echo "Changes detected. A redeployment will take place.";
+else
+  echo "No changes";
+  exit;
+fi
 
 # Stop web application process
 pm2 stop ServerApp
