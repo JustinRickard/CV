b62867f3e0a514065f6f58407c721b3b5b81c5fd
Justin Rickard <justinr86.jr@gmail.com>
2016-04-29 21:53:45 +0100

    Fix deploy script

--- DeployUpdate.sh
+++ DeployUpdate.sh
@@ -4,13 +4,11 @@
 # Author  : Justin Rickard
 # Changes : Add script to deploy updates to the app
 
-# JR: Nothing change to test auto deployment
-
 # Set up variables
 workingDirectory=~/websites/CV
 githubUrl=https://github.com/JustinRickard/CV.git
 
-cd $workingDirectory/CV
+cd $workingDirectory
 
 # Check if up-to-date
 result=$(git fetch --dry-run 2>&1)
